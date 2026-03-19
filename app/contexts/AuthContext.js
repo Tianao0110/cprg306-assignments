4c1291a7534274a657a8264e2ba24663f92a8048
Tianao0110 <123719125+Tianao0110@users.noreply.github.com>
2026-03-19 17:40:28 -0600

    add google to log in

--- app/contexts/AuthContext.js
+++ app/contexts/AuthContext.js
@@ -1,46 +1,53 @@
 "use client";
- 
+
 import { useContext, createContext, useState, useEffect } from "react";
 import {
   signInWithPopup,
   signOut,
   onAuthStateChanged,
   GithubAuthProvider,
+  GoogleAuthProvider,
 } from "firebase/auth";
 
-import { auth } from "../utils/firebase"; 
- 
+import { auth } from "../utils/firebase";
+
 const AuthContext = createContext();
- 
+
 export const AuthContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
- 
-  // LogIn GitHub
+
+  // Log in GitHub
   const gitHubSignIn = () => {
     const provider = new GithubAuthProvider();
     return signInWithPopup(auth, provider);
   };
- 
+
+  // Log in Google
+  const googleSignIn = () => {
+    const provider = new GoogleAuthProvider();
+    return signInWithPopup(auth, provider);
+  };
+
   // Log out
   const firebaseSignOut = () => {
     return signOut(auth);
   };
- 
-  
+
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
     });
     return () => unsubscribe();
   }, [user]);
- 
+
   return (
-    <AuthContext.Provider value={{ user, gitHubSignIn, firebaseSignOut }}>
+    <AuthContext.Provider
+      value={{ user, gitHubSignIn, googleSignIn, firebaseSignOut }}
+    >
       {children}
     </AuthContext.Provider>
   );
 };
- 
 
 export const useUserAuth = () => {
   return useContext(AuthContext);
