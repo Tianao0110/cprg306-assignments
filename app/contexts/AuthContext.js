53efb9f21a8a00d0c9d4de429653f448eb48e61e
Tianao0110 <123719125+Tianao0110@users.noreply.github.com>
2026-03-19 15:56:52 -0600

    Create AuthContext and useAuth Hook

---
+++ app/contexts/AuthContext.js
@@ -0,0 +1,47 @@
+"use client";
+ 
+import { useContext, createContext, useState, useEffect } from "react";
+import {
+  signInWithPopup,
+  signOut,
+  onAuthStateChanged,
+  GithubAuthProvider,
+} from "firebase/auth";
+
+import { auth } from "../utils/firebase"; 
+ 
+const AuthContext = createContext();
+ 
+export const AuthContextProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+ 
+  // LogIn GitHub
+  const gitHubSignIn = () => {
+    const provider = new GithubAuthProvider();
+    return signInWithPopup(auth, provider);
+  };
+ 
+  // Log out
+  const firebaseSignOut = () => {
+    return signOut(auth);
+  };
+ 
+  
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => unsubscribe();
+  }, [user]);
+ 
+  return (
+    <AuthContext.Provider value={{ user, gitHubSignIn, firebaseSignOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+ 
+
+export const useUserAuth = () => {
+  return useContext(AuthContext);
+};
